ca97b9cb0321f711c0b8fe851d31b8ca337c193a
Ahmed <ahmed@uney.com>
2026-01-21 11:05:32 +0400

    Fix music arrow: make it point straight down, bigger (5em), centered above music button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     password: '282025', // Change this to your desired password
     birthdayDate: new Date('2026-01-25').toDateString(), // January 25, 2026
-    testMode: false, // Set to true to unlock all events immediately for testing
+    testMode: true, // Set to true to unlock all events immediately for testing
     countdownTest: true, // Set to true to show countdown immediately for testing, false to wait for Jan 25
 };
 
